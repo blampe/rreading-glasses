580cf0aa8719accde0d5cc3e7a216e356f38df8b
Adam Traeger <complacentsee@gmail.com>
2025-07-07 16:40:03 -0500

    Normalize all gauge and count to vectors.

--- internal/prometheus.go
+++ internal/prometheus.go
@@ -13,15 +13,16 @@ import (
 
 var (
 	// strip all `{...}` segments from the pattern to build a label
-	pathParamRE                   = regexp.MustCompile(`\{[^/]+\}`)
-	_           HTTPMetrics       = (*httpMetrics)(nil)
-	_           HTTPMetrics       = (*nohttpmetrics)(nil)
-	_           ControllerMetrics = (*controllerMetrics)(nil)
-	_           ControllerMetrics = (*noControllerMetrics)(nil)
-	_           CacheMetrics      = (*cacheMetrics)(nil)
-	_           CacheMetrics      = (*noCacheMetrics)(nil)
-	_           GQLMetrics        = (*nogqlMetrics)(nil)
-	_           GQLMetrics        = (*gqlMetrics)(nil)
+	pathParamRE = regexp.MustCompile(`\{[^/]+\}`)
+
+	_ HTTPMetrics       = (*httpMetrics)(nil)
+	_ HTTPMetrics       = (*nohttpmetrics)(nil)
+	_ ControllerMetrics = (*controllerMetrics)(nil)
+	_ ControllerMetrics = (*noControllerMetrics)(nil)
+	_ CacheMetrics      = (*cacheMetrics)(nil)
+	_ CacheMetrics      = (*noCacheMetrics)(nil)
+	_ GQLMetrics        = (*nogqlMetrics)(nil)
+	_ GQLMetrics        = (*gqlMetrics)(nil)
 )
 
 type HTTPMetrics interface {
@@ -48,9 +49,10 @@ type ControllerMetrics interface {
 }
 
 type controllerMetrics struct {
-	operations *prometheus.GaugeVec
-	eTag       *prometheus.CounterVec
-	eTagRatio  prometheus.Gauge
+	controllerTotals  *prometheus.CounterVec
+	controllerWaiting *prometheus.GaugeVec
+	eTagTotals        *prometheus.CounterVec
+	eTagRatio         prometheus.Gauge
 }
 
 type noControllerMetrics struct{}
@@ -62,8 +64,7 @@ type CacheMetrics interface {
 }
 
 type cacheMetrics struct {
-	hits     prometheus.Counter
-	misses   prometheus.Counter
+	totals   *prometheus.CounterVec
 	hitRatio prometheus.Gauge
 }
 
@@ -76,8 +77,7 @@ type GQLMetrics interface {
 }
 
 type gqlMetrics struct {
-	batchesSent prometheus.Counter
-	queriesSent prometheus.Counter
+	totals *prometheus.CounterVec
 }
 
 type nogqlMetrics struct{}
@@ -119,16 +119,24 @@ func NewPrometheusMetrics(appName string) MetricsMiddleware {
 	)
 
 	// Controller Metrics
-	controllerOps := prometheus.NewGaugeVec(
+	controllerTotalOps := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: appName,
+			Subsystem: "controller",
+			Name:      "total_operations",
+			Help:      "Counts of total controller operations by type.",
+		},
+		[]string{"type"},
+	)
+	controllerWaitingOps := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Namespace: appName,
 			Subsystem: "controller",
 			Name:      "waiting_operations",
-			Help:      "Counts of controller operations by type.",
+			Help:      "Counts of waiting controller operations by type.",
 		},
 		[]string{"type"},
 	)
-
 	controllerEtag := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace: appName,
@@ -149,21 +157,14 @@ func NewPrometheusMetrics(appName string) MetricsMiddleware {
 	)
 
 	// Cache Metrics
-	cacheHits := prometheus.NewCounter(
+	cacheHits := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace: appName,
 			Subsystem: "cache",
-			Name:      "hits_total",
-			Help:      "Total number of cache hits.",
+			Name:      "total",
+			Help:      "Totals for cache system.",
 		},
-	)
-	cacheMisses := prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Namespace: appName,
-			Subsystem: "cache",
-			Name:      "misses_total",
-			Help:      "Total number of cache misses.",
-		},
+		[]string{"type"},
 	)
 	cacheHitRatio := prometheus.NewGauge(
 		prometheus.GaugeOpts{
@@ -173,21 +174,14 @@ func NewPrometheusMetrics(appName string) MetricsMiddleware {
 			Help:      "Ratio of cache hits to total cache operations.",
 		},
 	)
-	batchesSent := prometheus.NewCounter(
+	gqlclientCounters := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace: appName,
 			Subsystem: "gqlclient",
-			Name:      "total_batches_sent",
+			Name:      "total",
 			Help:      "How many batches have been sent.",
 		},
-	)
-	queriesSent := prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Namespace: appName,
-			Subsystem: "gqlclient",
-			Name:      "total_queries_sent",
-			Help:      "How many queries have been included across all batches.",
-		},
+		[]string{"type"},
 	)
 
 	// Register all
@@ -195,13 +189,15 @@ func NewPrometheusMetrics(appName string) MetricsMiddleware {
 		httpRequestDuration,
 		inFlight,
 
-		controllerOps,
+		controllerTotalOps,
+		controllerWaitingOps,
 		controllerEtag,
 		controllerETagRatio,
 
 		cacheHits,
-		cacheMisses,
 		cacheHitRatio,
+
+		gqlclientCounters,
 	)
 
 	return MetricsMiddleware{
@@ -211,18 +207,17 @@ func NewPrometheusMetrics(appName string) MetricsMiddleware {
 			requestInflight: inFlight,
 		},
 		Controller: &controllerMetrics{
-			operations: controllerOps,
-			eTagRatio:  controllerETagRatio,
-			eTag:       controllerEtag,
+			controllerTotals:  controllerTotalOps,
+			controllerWaiting: controllerWaitingOps,
+			eTagTotals:        controllerEtag,
+			eTagRatio:         controllerETagRatio,
 		},
 		Cache: &cacheMetrics{
-			hits:     cacheHits,
-			misses:   cacheMisses,
+			totals:   cacheHits,
 			hitRatio: cacheHitRatio,
 		},
 		GQL: &gqlMetrics{
-			batchesSent: batchesSent,
-			queriesSent: queriesSent,
+			totals: gqlclientCounters,
 		},
 	}
 }
@@ -276,11 +271,12 @@ func (*nohttpmetrics) HandleFunc(mux *http.ServeMux, pattern string, hf http.Han
 }
 
 func (cm *controllerMetrics) DenormWaitingInc() {
-	cm.operations.WithLabelValues("denorm_waiting").Inc()
+	cm.controllerWaiting.WithLabelValues("denorm_waiting").Inc()
 }
 
 func (cm *controllerMetrics) DenormWaitingDec() {
-	cm.operations.WithLabelValues("denorm_waiting").Dec()
+	cm.controllerWaiting.WithLabelValues("denorm_waiting").Dec()
+	cm.denormCompletedInc()
 }
 
 func (cm *controllerMetrics) DenormWaitingAdd(delta int64) {
@@ -288,18 +284,32 @@ func (cm *controllerMetrics) DenormWaitingAdd(delta int64) {
 		return
 	}
 	if delta < 0 {
-		cm.operations.WithLabelValues("denorm_waiting").Sub(float64(-delta))
+		cm.controllerWaiting.WithLabelValues("denorm_waiting").Sub(float64(-delta))
+		cm.denormCompletedAdd(-delta)
 	} else {
-		cm.operations.WithLabelValues("denorm_waiting").Add(float64(delta))
+		cm.controllerWaiting.WithLabelValues("denorm_waiting").Add(float64(delta))
+	}
+}
+
+func (cm *controllerMetrics) denormCompletedInc() {
+	cm.controllerTotals.WithLabelValues("denorm_completed").Inc()
+}
+
+func (cm *controllerMetrics) denormCompletedAdd(delta int64) {
+	if delta <= 0 {
+		return
+	} else {
+		cm.controllerTotals.WithLabelValues("denorm_completed").Add(float64(delta))
 	}
 }
 
 func (cm *controllerMetrics) RefreshWaitingInc() {
-	cm.operations.WithLabelValues("refresh_waiting").Inc()
+	cm.controllerWaiting.WithLabelValues("refresh_waiting").Inc()
 }
 
 func (cm *controllerMetrics) RefreshWaitingDec() {
-	cm.operations.WithLabelValues("refresh_waiting").Dec()
+	cm.controllerWaiting.WithLabelValues("refresh_waiting").Dec()
+	cm.refreshCompletedInc()
 }
 
 func (cm *controllerMetrics) RefreshWaitingAdd(delta int64) {
@@ -307,17 +317,30 @@ func (cm *controllerMetrics) RefreshWaitingAdd(delta int64) {
 		return
 	}
 	if delta < 0 {
-		cm.operations.WithLabelValues("refresh_waiting").Sub(float64(-delta))
+		cm.controllerWaiting.WithLabelValues("refresh_waiting").Sub(float64(-delta))
+		cm.refreshCompletedAdd(-delta)
 	} else {
-		cm.operations.WithLabelValues("refresh_waiting").Add(float64(delta))
+		cm.controllerWaiting.WithLabelValues("refresh_waiting").Add(float64(delta))
+	}
+}
+
+func (cm *controllerMetrics) refreshCompletedInc() {
+	cm.controllerTotals.WithLabelValues("refresh_completed").Inc()
+}
+
+func (cm *controllerMetrics) refreshCompletedAdd(delta int64) {
+	if delta <= 0 {
+		return
+	} else {
+		cm.controllerTotals.WithLabelValues("refresh_completed").Add(float64(delta))
 	}
 }
 
 func (cm *controllerMetrics) EtagMatchesInc() {
-	cm.eTag.WithLabelValues("matches").Inc()
+	cm.eTagTotals.WithLabelValues("matches").Inc()
 }
 func (cm *controllerMetrics) EtagMismatchesInc() {
-	cm.eTag.WithLabelValues("mismatches").Inc()
+	cm.eTagTotals.WithLabelValues("mismatches").Inc()
 }
 func (cm *controllerMetrics) EtagRatioSet(val float64) {
 	cm.eTagRatio.Set(val)
@@ -334,10 +357,10 @@ func (cm *noControllerMetrics) EtagMismatchesInc()            {}
 func (cm *noControllerMetrics) EtagRatioSet(val float64)      {}
 
 func (cm *cacheMetrics) CacheHitInc() {
-	cm.hits.Inc()
+	cm.totals.WithLabelValues("hits").Inc()
 }
 func (cm *cacheMetrics) CacheMissInc() {
-	cm.misses.Inc()
+	cm.totals.WithLabelValues("misses").Inc()
 }
 func (cm *cacheMetrics) CacheHitRatioSet(val float64) {
 	cm.hitRatio.Set(val)
@@ -348,21 +371,18 @@ func (cm *noCacheMetrics) CacheMissInc()                {}
 func (cm *noCacheMetrics) CacheHitRatioSet(val float64) {}
 
 func (gm *gqlMetrics) BatchesSentInc() {
-	gm.batchesSent.Inc()
+	gm.totals.WithLabelValues("batches_sent").Inc()
 }
 
 func (gm *gqlMetrics) QueriesSentInc() {
-	gm.queriesSent.Inc()
+	gm.totals.WithLabelValues("queries_sent").Inc()
 }
 
 func (gm *gqlMetrics) QueriesSentAdd(delta int64) {
-	if delta == 0 {
+	if delta <= 0 {
 		return
-	}
-	if delta < 0 {
-		gm.queriesSent.Add(float64(-delta))
 	} else {
-		gm.queriesSent.Add(float64(delta))
+		gm.totals.WithLabelValues("queries_sent").Add(float64(delta))
 	}
 }
 
